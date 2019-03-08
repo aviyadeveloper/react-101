adb17a2a4a8e3c45d9d19533982009d45f05cea5
Aviya Developer <aviya.developer@gmail.com>
2019-03-08 19:48:18 +0200

    Fixed Header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,9 @@ interface HeaderProps {
 
 export const Header = (props: HeaderProps) => (
   <div className="header">
-    <h1 className="header__title">{props.title}</h1>
+    <h1 className="header__title">
+      RAND<span className="header__bright-primary">OCRACY</span>
+    </h1>
     <h2 className="header__subtitle">{props.subtitle}</h2>
   </div>
 );
